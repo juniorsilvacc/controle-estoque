fb81312afc98597ccee9fe56356e48fe4f32f3ec
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-10-11 16:50:28 -0300

    adicionando a foto do produto na view de detalhes de produto

--- resources/views/user/produtos/detalhes-produtos.blade.php
+++ resources/views/user/produtos/detalhes-produtos.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Detalhes Produto')
 
 @section('content')
-    <div class="container-fluid px-4 details">
+    {{-- <div class="container-fluid px-4 details">
         <div class="row bg-details">
             <div class="col title-details">
                 <h1>Detalhes Produto</h1>
@@ -29,6 +29,53 @@
                 @include('user.produtos.partials.delete-modal')
 
             </form>
+        </div>
+
+    </div> --}}
+    <div class="container details">
+        <div class="row bg-details">
+            <div class="col-md-7 product-details">
+
+                <div class="col title-details">
+                    <h1>Detalhes Produto</h1>
+                </div>
+
+                <div class="informations">
+                    <h6><strong>Nome:</strong> {{ $produto->nome }} </h6>
+                    <h6><strong>Código Referência:</strong> {{ $produto->cod_referencia }} </h6>
+                    <h6><strong>Descrição:</strong>
+                        @empty($produto->descricao)
+                            Sem descrição
+                        @else
+                            {{ $produto->descricao }}
+                        @endempty
+                    </h6>
+                    <h6><strong>Unidade de Médida:</strong> {{ $produto->unidade_medida }} </h6>
+                    <h6><strong>Preço Unitário:</strong> R$ {{ $produto->preco_unitario }} </h6>
+                    <h6><strong>Estoque:</strong> {{ $produto->estoque }} </h6>
+                    <h6><strong>Data Fabricação:</strong> {{ $produto->data_fabricacao }} </h6>
+                    <h6><strong>Data Validade:</strong> {{ $produto->data_validade }} </h6>
+                    <h6><strong>Fornecedor:</strong> {{ $produto->fornecedor['nome'] }} </h6>
+                    <h6><strong>Categoria:</strong> {{ $produto->categoria['nome'] }} </h6>
+                </div>
+
+                <form class="form-delete" action="{{ route('produtos.deleteAction', $produto->id) }}" method="POST">
+
+                    @method('delete')
+                    @csrf
+
+                    @include('user.produtos.partials.delete-modal')
+
+                </form>
+            </div>
+
+            <div class="col-md-5 user-photo text-center">
+                <div class="col title-details">
+                    <h1>Foto</h1>
+                </div>
+                <img src="{{ $produto->image ? asset("storage/{$produto->image}") : asset('img/sem-foto.jpg') }}"
+                    alt="{{ $produto->nome }}" class="foto-details-product">
+            </div>
         </div>
     </div>
 @endsection
